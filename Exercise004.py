0a14015073e8726e20b051b0582442579d4bf62e
Muminov Kamoljon <kamol.mumino.786@gmail.com>
2022-09-28 11:49:03 +0300

    Exercise005

---
+++ Exercise004.py
@@ -0,0 +1,12 @@
+quarter = int( input())
+
+if quarter == 1:
+    print("x > 0, y > 0")
+elif quarter == 2:
+    print("x < 0, y > 0")
+elif quarter == 3:
+    print("x < 0, y < 0")
+elif quarter == 4:
+    print("x > 0, y < 0")
+else:
+    print("Error, wrong number!")
